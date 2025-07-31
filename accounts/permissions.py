c4131e3cda2e670c30ec52c5575bdfa5e55d8b54
roshan-sk <151342528+roshan-sk@users.noreply.github.com>
2025-07-31 09:46:43 +0530

    Delete accounts directory

--- accounts/permissions.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.permissions import BasePermission
-
-class IsAppAdmin(BasePermission):
-    """
-    Allows access only to app owner (admin).
-    """
-
-    def has_permission(self, request, view):
-        return request.user and request.user.is_authenticated and request.user.is_app_admin
